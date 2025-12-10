ce1182f1a1ed826613e78d6618c6e60537ea4a42
慰めのため <150973942+NagusameCS@users.noreply.github.com>
2025-12-10 18:30:48 +0000

    push 2

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import './style.css'
 import { Game } from './Game'
 
-new Game();
+console.log('Starting Game...');
+try {
+    new Game();
+    console.log('Game started successfully.');
+} catch (e) {
+    console.error('Failed to start game:', e);
+}
 
